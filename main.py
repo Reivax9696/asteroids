810fe965dd4720dce6b386375d3e2c67f4ef8388
Reivax9696 <reivax9696@gmail.com>
2025-02-21 13:13:45 +0100

    added sprite for the player, now player can rotate and move forward

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import pygame
 import constants
+from player import Player
 
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    player1 = Player(x=constants.SCREEN_WIDTH / 2, y=constants.SCREEN_HEIGHT / 2)
+    updatable.add(player1)
+    drawable.add(player1)
+    running = True  
+    
+    while running:
+        dt = clock.tick(60) / 1000.0
+        
+        for event in pygame.event.get():    
+            if event.type == pygame.QUIT:
+                running = False
+        
+		#Update player situation
+        updatable.update(dt)
+        
+        screen.fill((0, 0, 0))
+        
+		#Draw the sprites in drawable
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+        # Update the display
+        pygame.display.flip()
+        clock.tick(60)
 
-
-def main ():
-	pygame.init()
-	screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-	number = True
-	while number == True:
-		for event in pygame.event.get():
-   			 if event.type == pygame.QUIT:
-        		 	return	
-		screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-		screen.fill((0, 0, 0))
-		pygame.display.flip()
-
-	
-	
-
-
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,41 @@
+import pygame
+from circleshape import CircleShape
+import constants
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, constants.PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        triangle_points = self.triangle()
+        pygame.draw.polygon(screen, (255, 255, 255), [(int(p.x), int(p.y)) for p in triangle_points], 2)
+        
+    def rotate (self, dt, direction):
+        self.rotation += constants.PLAYER_TURN_SPEED * dt * direction
+        
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt, direction=-1)
+        if keys[pygame.K_d]:
+            self.rotate(dt, direction=1)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt)
+    
+    def move (self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
+            
+            
